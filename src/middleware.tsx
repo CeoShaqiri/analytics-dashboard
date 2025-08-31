3dff88783be60d66f6a0d60cbc92638cf5555e4a
Sena <senavisionstudio@gmail.com>
2025-08-31 08:28:58 +0200

    still setting up middleware

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,5 +1,13 @@
 import { NextRequest } from "next/server";
 
 export default async function middleware(req:NextRequest) {
-    
+    //checking where the user is navigating if on homepage treating like a visitor
+    if(req.nextUrl.pathname === '/'){
+        //track analytics event
+        console.log("Track!")
+    }
+}
+// The matcher determine when the middleware runs on which path
+export const matcher ={
+    matcher:['/']
 }
