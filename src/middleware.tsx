7372fd703768e104a5aae6e9a99765041ae9ba37
Sena <senavisionstudio@gmail.com>
2025-08-31 10:09:27 +0200

    debug

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -3,19 +3,15 @@ import { analytics } from "./utils/analytics";
 
 export default async function middleware(req:NextRequest) {
     //checking where the user is navigating if on homepage treating like a visitor
-    if(req.nextUrl.pathname === '/'){
-        //track analytics event
-     try {
-  analytics.track("pageview", {
-    page: '/',
-    
-  })
-} catch (error) {
-  console.error("Analytics error:", error)
+   if (req.nextUrl.pathname === '/') {
+  try {
+    analytics.track("pageview", { page: '/' })
+  } catch (error) {
+    console.error("Analytics error:", error)
+  }
 }
-    }
+return NextResponse.next()
 }
-// The matcher determine when the middleware runs on which path
-export const matcher ={
-    matcher:['/']
+export const config = {
+  matcher: ["/"], // this runs middleware only on homepage
 }
